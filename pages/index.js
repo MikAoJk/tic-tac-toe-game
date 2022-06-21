b5d477930015876514e7d0eeb37eb1e2c465330a
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2022-06-21 09:06:01 +0200

    fixed import

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { prefix } from '../../utils/prefix.js';
+import { prefix } from '../utils/prefix.js';
 
 export default function Home() {
   return (
