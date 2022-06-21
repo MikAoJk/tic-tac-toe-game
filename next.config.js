4849c670a1f5abf7862195a35550dd959142ad8c
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2022-06-21 10:08:40 +0200

    trying to make it work on github pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-}
 
-module.exports = nextConfig
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = {
+  /**
+   * Tell Next.js where the `public` folder is.
+   * Replace `nextjs-github-pages` with your Github repo project name.
+   */
+  assetPrefix: isProd ? "/tic-tac-toe-game/" : ""
+};
