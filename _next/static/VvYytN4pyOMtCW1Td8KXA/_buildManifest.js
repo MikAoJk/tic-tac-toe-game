2004b41b9a162c32babed44cef7b295e8e5e8a12
MikAoJk <joakimkartveit@gmail.com>
2022-06-24 07:50:57 +0000

    Deploying to gh-pages from @ MikAoJk/tic-tac-toe-game@f5fab16c5c56ae41a48118973b14946398df266f 🚀

--- _next/static/VvYytN4pyOMtCW1Td8KXA/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ed8752a7cce4fc71.css","static/chunks/pages/index-eed4dd46e3519ead.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/g3Mf2ucuA2CIUjvboSefr/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ed8752a7cce4fc71.css","static/chunks/pages/index-0766b81ef27c7996.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
