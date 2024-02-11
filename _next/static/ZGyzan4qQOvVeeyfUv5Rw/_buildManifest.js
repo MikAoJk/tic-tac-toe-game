d9ba26c02b1cf2bbd92102c253b32d9e8ede4047
MikAoJk <joakimkartveit@gmail.com>
2024-02-11 19:53:39 +0000

    Deploying to gh-pages from @ MikAoJk/tic-tac-toe-game@3275cbf4138ea225eba5b6ba490799ecbbf91a55 🚀

--- _next/static/ZGyzan4qQOvVeeyfUv5Rw/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/0af0820a41f2d9a0.css","static/chunks/pages/index-d9f0d8c839469c18.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
